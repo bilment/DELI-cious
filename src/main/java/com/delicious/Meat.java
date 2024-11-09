2c9e6c5e851a30fb25f54bcd63d7789e887780f9
Bilment <tahabilmen@gmail.com>
2024-11-08 20:10:29 -0500

    #4 Meat child class has been added.

---
+++ src/main/java/com/delicious/Meat.java
@@ -0,0 +1,13 @@
+package com.delicious;
+
+public class Meat extends Topping {
+
+    public Meat(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(String size) {
+        
+    }
+}
