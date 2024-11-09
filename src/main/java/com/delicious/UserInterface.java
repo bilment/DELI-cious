42f56079db27293a963dda1f64100cc02151a249
Bilment <tahabilmen@gmail.com>
2024-11-08 20:42:21 -0500

    #11 UserInterface class has been added.

---
+++ src/main/java/com/delicious/UserInterface.java
@@ -0,0 +1,23 @@
+package com.delicious;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner scanner;
+
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void showMainMenu() {}
+
+    public void showOrderMenu() {}
+
+    private void addSandwichToOrder() {}
+
+    private void addDrinkToOrder() {}
+
+    private void addChipsToOrder() {}
+
+    private void checkoutOrder() {}
+}
