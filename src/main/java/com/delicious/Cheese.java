d8fef45b659aedfd6be2f4095cdebe2ea4c1e10f
Bilment <tahabilmen@gmail.com>
2024-11-08 20:13:33 -0500

    #5 Cheese child class has been added.

---
+++ src/main/java/com/delicious/Cheese.java
@@ -0,0 +1,13 @@
+package com.delicious;
+
+public class Cheese extends Topping {
+
+    public Cheese(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice(String size) {
+        
+    }
+}
