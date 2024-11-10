86e49f57fc31e1b8d693b1582851b4e6f86850dd
Bilment <tahabilmen@gmail.com>
2024-11-10 18:33:28 -0500

    #13 Cheese class has been completed.

--- src/main/java/com/delicious/Cheese.java
+++ src/main/java/com/delicious/Cheese.java
@@ -8,6 +8,15 @@ public class Cheese extends Topping {
 
     @Override
     public double calculatePrice(String size) {
-
+        switch (size) {
+            case "4":
+                return 0.75;
+            case "8":
+                return 1.50;
+            case "12":
+                return 2.25;
+            default:
+                return 0.00;
+        }
     }
 }
