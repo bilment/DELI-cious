3e72e774711bbda076c5b45baaaa5a6ec277e334
Bilment <tahabilmen@gmail.com>
2024-11-10 18:31:22 -0500

    #12 Meat class has been completed.

--- src/main/java/com/delicious/Meat.java
+++ src/main/java/com/delicious/Meat.java
@@ -8,6 +8,15 @@ public class Meat extends Topping {
 
     @Override
     public double calculatePrice(String size) {
-        
+        switch (size) {
+            case "4":
+                return 1.00;
+            case "8":
+                return 2.00;
+            case "12":
+                return 3.00;
+            default:
+                return 0.00;
+        }
     }
 }
