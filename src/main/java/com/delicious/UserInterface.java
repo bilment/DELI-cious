8865a34ecc273e31b0d778d38430e2f244ba95c0
Bilment <tahabilmen@gmail.com>
2024-11-12 10:36:10 -0500

    #25 UserInterface class, showOrderMenu method has been completed.

--- src/main/java/com/delicious/UserInterface.java
+++ src/main/java/com/delicious/UserInterface.java
@@ -16,7 +16,43 @@ public class UserInterface {
         System.out.print("Your choice: ");
     }
 
-    public void showOrderMenu() {}
+    public void showOrderMenu(Order order) {
+        boolean ordering = true;
+
+        while (ordering) {
+            System.out.println("Order Menu:");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addSandwichToOrder(order);
+                    break;
+                case 2:
+                    addDrinkToOrder(order);
+                    break;
+                case 3:
+                    addChipsToOrder(order);
+                    break;
+                case 4:
+                    checkoutOrder(order);
+                    ordering = false;
+                    break;
+                case 0:
+                    System.out.println("Order cancelled.");
+                    ordering = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 
     private void addSandwichToOrder() {}
 
