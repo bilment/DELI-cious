d7bdce9ff21986b346598992d9d4c17c1cffa5c3
Bilment <tahabilmen@gmail.com>
2024-11-13 11:05:18 -0500

    #38 BLT class has been completed.

--- src/main/java/com/delicious/BLT.java
+++ src/main/java/com/delicious/BLT.java
@@ -1,4 +1,14 @@
 package com.delicious;
 
-public class BLT {
+public class BLT extends Sandwich {
+
+    public BLT() {
+        super("8", "White", true);
+
+        this.addPremiumTopping(new Meat("Bacon"));
+        this.addPremiumTopping(new Cheese("Cheddar"));
+        this.addRegularTopping(new RegularTopping("Lettuce"));
+        this.addRegularTopping(new RegularTopping("Tomato"));
+        this.addRegularTopping(new RegularTopping("Ranch"));
+    }
 }
