64d93f809d31839e550f71dcc46417de6bc2b3bb
Bilment <tahabilmen@gmail.com>
2024-11-13 11:18:55 -0500

    #39 PhillyCheeseSteak class has been completed.

--- src/main/java/com/delicious/PhillyCheeseSteak.java
+++ src/main/java/com/delicious/PhillyCheeseSteak.java
@@ -1,4 +1,13 @@
 package com.delicious;
 
-public class PhillyCheeseSteak {
+public class PhillyCheeseSteak extends Sandwich {
+
+    public PhillyCheeseSteak() {
+        super("8", "White", true);
+
+        this.addPremiumTopping(new Meat("Steak"));
+        this.addPremiumTopping(new Cheese("American"));
+        this.addRegularTopping(new RegularTopping("Peppers"));
+        this.addRegularTopping(new RegularTopping("Mayo"));
+    }
 }
